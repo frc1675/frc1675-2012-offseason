d00b67b9c7809957932df3921c7fe455dc8abb9f
Eric Miller <agent.17k@gmail.com>
2012-12-06 18:47:19 -0600

    TankDrive classes

---
+++ src/org/frc1675/subsystems/TankDrive.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.frc1675.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author ericmiller
+ */
+public class TankDrive extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
